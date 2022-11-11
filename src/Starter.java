f62325c75e82541ef6ba04141147d6df76157ddd
Aaron Block <adblock@gmail.com>
2022-11-11 10:36:42 -0600

    Ask for name

--- src/Starter.java
+++ src/Starter.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class Starter {
     public static void main(String[] args) {
+        System.out.println("Please enter your name");
+        Scanner scan = new Scanner(System.in);
+        String name = scan.nextLine();
+        System.out.println("Your name is "+name);
+
         System.out.println("Hello world Yay this is working");
         System.out.println("I'm in the main now");
     }
