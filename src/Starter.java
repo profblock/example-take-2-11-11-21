1cd92311c7677fe02b2514e7876e18381c5fd60c
Aaron Block <adblock@gmail.com>
2022-11-11 10:33:30 -0600

    Gonna send this to the main

--- src/Starter.java
+++ src/Starter.java
@@ -1,5 +1,6 @@
 public class Starter {
     public static void main(String[] args) {
         System.out.println("Hello world Yay this is working");
+        System.out.println("I'm in the main now");
     }
 }
