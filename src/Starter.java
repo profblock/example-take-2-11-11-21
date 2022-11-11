3990ac7e051eb217a841c066add6e70d67a839ae
Aaron Block <adblock@gmail.com>
2022-11-11 10:35:26 -0600

    Ask for integerg

--- src/Starter.java
+++ src/Starter.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
+
 public class Starter {
     public static void main(String[] args) {
         System.out.println("Hello world Yay this is working");
         System.out.println("I'm in the other branch");
+
+
+        System.out.println("Please enter an integer");
+        Scanner scan = new Scanner(System.in);
+        int x = scan.nextInt();
+        System.out.println("You entered "+x);
     }
 }
